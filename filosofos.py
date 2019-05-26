073b32ef5da81a99e59a8732b9f83859d4c012ed
WilliamCadenas21 <williamcadenas21@gmail.com>
2019-05-25 23:14:28 -0500

    padilla soluciono el bug

--- filosofos.py
+++ filosofos.py
@@ -3,6 +3,7 @@ from texttable import Texttable
 import random
 import numpy as np 
 import time
+import sys
 
 comm = MPI.COMM_WORLD 
 rank = comm.Get_rank()
@@ -26,18 +27,20 @@ if rank == 0:
 
     t.add_rows([typeOfPhilosopher,info1,info2])
 
-    for i in range(0,7):
+    count = 0
+    while True:
+        count += 1
         time.sleep(1)
-        print('tabla',i)
-        print (t.draw())
-        
-
-        
+        #print('table: ',count)
+        #print (t.draw())
+        sys.stdout.flush()
 
 else:
-    print('acabo de inciar ',rank-1)    
+    #print('acabo de inciar ',rank)    
     #while True :
     rand = random.randrange(7,10)
-        #time.sleep(rand)
-    #    print('soy el proceso ',rank,' y acabo de desertar')
-    print('rand: ', rand)
+    print('soy el proceso ',rank,'y voy a dormir ', rand)
+    sys.stdout.flush()
+    time.sleep(rand)
+    print('soy el proceso ',rank,' y acabo de desertar')
+    sys.stdout.flush()
